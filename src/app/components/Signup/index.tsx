c5383627259bc2979e519e2d71fa7fa07f265f9c
varungupta2906 <varun.gupta@primathon.in>
2023-10-12 19:48:19 +0530

    feature/signup-validations done

--- src/app/components/Signup/index.tsx
+++ src/app/components/Signup/index.tsx
@@ -2,8 +2,14 @@
 import React, { useState } from "react";
 import cx from "classnames";
 import Link from "next/link";
-import styles from "./styles.module.scss";
 import Toaster from "../Toaster";
+import {
+  EMAIL_CHECK_REGEX,
+  MOBILE_PHONE_REGEX,
+  PASSWORD_REGEX,
+} from "@/utils/constants";
+
+import styles from "./styles.module.scss";
 
 const SignUpPage = () => {
   const [email, setEmail] = useState("");
@@ -14,19 +20,74 @@ const SignUpPage = () => {
   const [isOpenToaster, setIsOpenToaster] = useState<boolean>(false);
   const [toasterMessage, setToasterMessage] = useState<string>("");
   const [isShowError, setIsShowError] = useState<boolean>(false);
+  const [image, setImage] = useState("");
+
+  const validations = (name: string, value: string) => {
+    if (name === "email") {
+      return EMAIL_CHECK_REGEX?.test(value as string);
+    }
+    if (name === "phoneNumber") {
+      return MOBILE_PHONE_REGEX?.test(value as string);
+    }
+    if (name === "password") {
+      return PASSWORD_REGEX?.test(value as string);
+    }
+
+    if (name === "confirm-password") {
+      return PASSWORD_REGEX?.test(value as string);
+    }
+  };
 
   const handleInputChange = (e: any) => {
     const { name, value } = e.target;
     if (name === "email") {
-      setEmail(value);
+      if (validations(name, value)) {
+        setIsOpenToaster(true);
+        setToasterMessage("Valid Email");
+        setIsShowError(false);
+        setEmail(value);
+      } else {
+        setIsOpenToaster(true);
+        setToasterMessage("Invalid Email");
+        setIsShowError(true);
+      }
     } else if (name === "password") {
-      setPassword(value);
+      if (validations(name, value)) {
+        setIsOpenToaster(true);
+        setToasterMessage("Valid Password");
+        setIsShowError(false);
+        setPassword(value);
+      } else {
+        setIsOpenToaster(true);
+        setToasterMessage("Invalid Password");
+        setIsShowError(true);
+      }
     } else if (name === "name") {
       setName(value);
     } else if (name === "phoneNumber") {
-      setPhoneNumber(value);
+      if (validations(name, value)) {
+        setIsOpenToaster(true);
+        setToasterMessage("Valid Phone Number");
+        setIsShowError(false);
+        setPhoneNumber(value);
+      } else {
+        setIsOpenToaster(true);
+        setToasterMessage("Invalid Phone Number");
+        setIsShowError(true);
+      }
     } else if (name === "confirm-password") {
-      setConfirmPassword(value);
+      if (validations(name, value)) {
+        setIsOpenToaster(true);
+        setToasterMessage("Valid Confirm Password");
+        setIsShowError(false);
+        setConfirmPassword(value);
+      } else {
+        setIsOpenToaster(true);
+        setToasterMessage("Invalid Confirm Password");
+        setIsShowError(true);
+      }
+    } else if (name === "file") {
+      setImage(value);
     }
   };
   const isDisabled = !(
@@ -34,7 +95,8 @@ const SignUpPage = () => {
     password.length > 0 &&
     name.length > 0 &&
     phoneNumber.length > 0 &&
-    confirmPassword.length > 0
+    confirmPassword.length > 0 &&
+    image.length > 0
   );
 
   const handleSubmit = () => {
@@ -43,6 +105,7 @@ const SignUpPage = () => {
       setToasterMessage("Passwords do not match");
       setIsShowError(true);
     } else {
+      console.log(image);
       setIsOpenToaster(true);
       setIsShowError(false);
       setToasterMessage("Signup successfull");
@@ -53,7 +116,7 @@ const SignUpPage = () => {
     <>
       <div className={styles.mainContainer}>
         <div className={styles.container}>
-          <p className={styles.heading}>LOGIN</p>
+          <p className={styles.heading}>CHATTING APP SIGN UP</p>
           <div className={styles.wrapper}>
             <input
               type="text"
@@ -69,6 +132,7 @@ const SignUpPage = () => {
               placeholder="Enter your Phone Number"
               onChange={handleInputChange}
             />
+
             <input
               type="email"
               name="email"
@@ -79,7 +143,7 @@ const SignUpPage = () => {
             <input
               type="password"
               name="password"
-              placeholder="Enter your password"
+              placeholder="Enter your password (Min. 8 characters)"
               onChange={handleInputChange}
             />
 
@@ -89,6 +153,17 @@ const SignUpPage = () => {
               placeholder="Re-enter your password"
               onChange={handleInputChange}
             />
+            <div>
+              <input
+                type="file"
+                name="file"
+                onChange={handleInputChange}
+                accept="Image/*"
+                className={styles.imageUpload}
+                required
+              />
+            </div>
+
             <button
               type="button"
               disabled={isDisabled}
@@ -97,7 +172,7 @@ const SignUpPage = () => {
               }
               onClick={handleSubmit}
             >
-              Sign Up
+              Create User
             </button>
 
             <p>
